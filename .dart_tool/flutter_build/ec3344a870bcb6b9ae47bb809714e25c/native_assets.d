fe84ae04b422a6bc0eb55ffbf3bbbd443d7459cf
sachin <sachinsingh1781998@gmail.com>
2024-12-15 10:52:21 +0530

    changed folder structure

---
+++ .dart_tool/flutter_build/ec3344a870bcb6b9ae47bb809714e25c/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\mark_1\\mark_1\\.dart_tool\\flutter_build\\ec3344a870bcb6b9ae47bb809714e25c\\native_assets.yaml: 
---
+++ app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'STARKI',
+            style: TextStyle(
+              color: Colors.white54,
+              fontStyle: FontStyle.italic,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
