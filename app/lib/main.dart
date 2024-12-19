e1e03b31ff97b365170ff45656ff43329d346cc7
sachin <sachinsingh1781998@gmail.com>
2024-12-19 21:00:56 +0530

    added dummy code

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'dart:html' as html;
 
 void main() {
   runApp(const MyApp());
@@ -16,17 +19,72 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         backgroundColor: Colors.black,
         body: Center(
-          child: Text(
-            'STARKI...',
-            style: TextStyle(
-              color: Colors.white54,
-              fontStyle: FontStyle.italic,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: TestButton(),
         ),
       ),
+    );
+  }
+}
+
+class TestButton extends StatefulWidget {
+  const TestButton({super.key});
+
+  @override
+  State<TestButton> createState() => _TestButtonState();
+}
+
+class _TestButtonState extends State<TestButton> {
+  bool isLoading = false;
+  late html.WindowBase window;
+
+  void onTap() {
+    setState(() {
+      isLoading = true;
+    });
+    final window = html.window.open('https://www.wikipedia.org/', '_blank');
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (window.closed!) {
+        setState(() {
+          isLoading = false;
+        });
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 100,
+        ),
+        SizedBox(
+          height: 50,
+          width: 200,
+          child: ElevatedButton(
+            onPressed: onTap,
+            child: isLoading
+                ? CircularProgressIndicator(
+                    backgroundColor: Colors.blueAccent,
+                    valueColor: AlwaysStoppedAnimation(Colors.redAccent),
+                    strokeWidth: 10,
+                  )
+                : Text(
+                    'Open Wikepedia',
+                    style: TextStyle(color: Colors.black),
+                  ),
+          ),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Text(
+          isLoading ? 'Wikipedia is Open' : 'Wikepedia is closed',
+          style: TextStyle(color: Colors.amberAccent),
+        )
+      ],
     );
   }
 }
